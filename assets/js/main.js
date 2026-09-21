4ba5e43d35466c22ab46e74b63b9bd451fa624fa
holi <12849675@qq.com>
2026-09-21 20:15:59 +0800

    Simplify linked course cards and widen CTA spacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,9 +262,7 @@ function renderCourses(data) {
         '<span class="course-badge ' + badgeClass + '">' + escapeHTML(c.level) + '</span>' +
         '<p class="course-name">' + escapeHTML(c.name) + '</p>';
       if (c.url) {
-        var host = c.url.replace(/^https?:\/\//, '').replace(/\/.*$/, '');
-        inner += '<p class="course-site">' + escapeHTML(host) + '</p>' +
-          '<span class="course-cta">Course Website' +
+        inner += '<span class="course-cta">Course Website' +
           '<svg width="12" height="12" fill="none" stroke="currentColor" viewBox="0 0 24 24">' +
           '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/></svg></span>';
         return '<a class="course-card course-card-link" href="' + escapeHTML(c.url) + '" target="_blank" rel="noopener">' + inner + '</a>';
